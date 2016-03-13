aea0598daa9a942a2559110fc43c06c11333ec0e
gertner <gertner@mitre.org>
2016-03-13 16:00:10 -0400

    updated comment

--- run_analysis.R
+++ run_analysis.R
@@ -1,5 +1,17 @@
 library(plyr)
 
+# This script reads the Samsung Galaxy S Smartphone Accelerometer data from 
+# http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones 
+# and produces a data frame summarizing the means of a subset of the features by subject and activity. 
+# 
+# The main function for this program is create.dataset(). Calling this function will perform the following steps: 
+#     
+# 1. Read the training and test data into a single combined dataset
+# 2. Filter the data to include only the features that represent mean() and std() 
+# 3. Rename the activity factor values as human readable factor names
+# 4. Create a new data frame that summarizes the average for all of the numeric feature values for each 
+# subject and activity
+# 5. Assign meaningful names to all features in the new data frame
 
 # Get the merged datasets and then create a new dataset that creates a second, 
 # independent tidy data set with the average of each variable for each activity and each subject.
